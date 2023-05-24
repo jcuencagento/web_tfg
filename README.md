883ac95e05858a8358cbdb7fa9cc3fab8f6a18dc
jcuencagento <jcuencagento@gmail.com>
2023-05-24 18:14:55 +0200

    First requests try 🔧

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Vision APIs Comparator Web
-Web page for Trabajo Fin de Grado de Javier Cuenca Gento en la UC3M.
+Página web para el Trabajo Fin de Grado de Javier Cuenca Gento en la UC3M.
--- src/lib/requests.js
+++ src/lib/requests.js
@@ -1,174 +1,106 @@
 import { error } from './logger';
+const translate = require('@vitalets/google-translate-api');
+const AWS = require('aws-sdk');
+const { ComputerVisionClient } = require('@azure/cognitiveservices-computervision');
+const { CognitiveServicesCredentials } = require('@azure/ms-rest-azure-js');
 
-const fetchAmazonAPI = (image, token) => {
-    new Promise((resolve, reject) => {
-        const url = new URL();  //URL here
-        opts.headers = opts.headers || {};
-        opts.headers.Authorization = `Bearer ${token}`; //Authorization whatever it is
+//Amazon Web Services
+const aws_access_key_id = 'AKIATXTZYMA46LDSKPEM';
+const aws_secret_access_key = '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';
+const region_name = 'us-east-2';
+AWS.config.update({
+    accessKeyId: aws_access_key_id,
+    secretAccessKey: aws_secret_access_key,
+    region: region_name
+});
+const reko_client = new AWS.Rekognition();
 
-        if (opts.params) {
-            if (opts.params instanceof Object || opts.params instanceof Array) {
-                url.search = new URLSearchParams(opts.params);
+//Google Cloud
+const { ImageAnnotatorClient } = require('@google-cloud/vision');
+const credentials = require('../assets/key.json');
+const vision_client = new ImageAnnotatorClient({ credentials });
+
+// Microsoft Azure
+const endpoint = 'https://computer-vision-api-prueba.cognitiveservices.azure.com/';
+const api_key = 'b3d63ad6b38c473982f5aca21a7ce948';
+const cv_client = new ComputerVisionClient(new CognitiveServicesCredentials(api_key), endpoint);
+
+const fetchAmazonAPI = (image) => {
+    return new Promise((resolve, reject) => {
+        const params = {
+            Image: {
+                Bytes: image // The image data in bytes or ArrayBuffer
+            },
+            MaxLabels: 8,
+            MinConfidence: 60
+        };
+
+        reko_client.detectLabels(params, (err, data) => {
+            if (err) {
+                reject(err);
             } else {
-                url.search = new URLSearchParams(Object.assign({}, opts.params));
+                const labels = data.Labels;
+                resolve(labels);
             }
-        }
-
-        return fetch(url, opts)
-            .then(response => {
-                if (response.status === 401) {
-                    return response
-                        .json()
-                        .then(body => {
-                            const err = new Error(`Request failed: ${body.msg}`);
-                            err.code = '401';
-                            error(err);
-                            reject(err);
-                        })
-                        .catch(err => reject(err));
-                }
-
-                if (response.ok) {
-                    const content_type = response.headers.get('content-type');
-                    if (content_type && content_type.indexOf('application/json' !== -1)) {
-                        return response
-                            .json()
-                            .then(body => resolve(body))
-                            .catch(err => reject(err));
-                    }
-
-                    // If the response is not a JSON we consider it an emtpy response
-                    return resolve();
-                }
-
-                return response
-                    .json()
-                    .then(body => {
-                        const error = new Error(`Request failed: ${body.msg}`);
-                        error.code = response.status;
-                        reject(error);
-                    })
-                    .catch(err => reject(err));
-            })
-            .catch(err => reject(err))
-    })
+        });
+    });
 };
 
-const fetchGoogleAPI = (image, token) => {
-    new Promise((resolve, reject) => {
-        const url = new URL();  //URL here
-        opts.headers = opts.headers || {};
-        opts.headers.Authorization = `Bearer ${token}`; //Authorization whatever it is
-
-        if (opts.params) {
-            if (opts.params instanceof Object || opts.params instanceof Array) {
-                url.search = new URLSearchParams(opts.params);
-            } else {
-                url.search = new URLSearchParams(Object.assign({}, opts.params));
-            }
-        }
-
-        return fetch(url, opts)
-            .then(response => {
-                if (response.status === 401) {
-                    return response
-                        .json()
-                        .then(body => {
-                            const err = new Error(`Request failed: ${body.msg}`);
-                            err.code = '401';
-                            error(err);
-                            reject(err);
-                        })
-                        .catch(err => reject(err));
-                }
-
-                if (response.ok) {
-                    const content_type = response.headers.get('content-type');
-                    if (content_type && content_type.indexOf('application/json' !== -1)) {
-                        return response
-                            .json()
-                            .then(body => resolve(body))
-                            .catch(err => reject(err));
-                    }
-
-                    // If the response is not a JSON we consider it an emtpy response
-                    return resolve();
-                }
-
-                return response
-                    .json()
-                    .then(body => {
-                        const error = new Error(`Request failed: ${body.msg}`);
-                        error.code = response.status;
-                        error(error);
-                        reject(error);
-                    })
-                    .catch(err => reject(err));
+const fetchGoogleAPI = (image) => {
+    return new Promise((resolve, reject) => {
+        const buffer = Buffer.from(image);
+        const requestImage = { content: buffer };
+        vision_client
+            .labelDetection(requestImage)
+            .then(([google_labels]) => {
+                const labels = google_labels.labelAnnotations;
+                const descriptions = labels.slice(0, 5).map((label) => label.description).join(', ');
+                resolve(descriptions);
             })
-            .catch(err => reject(err))
-    })
+            .catch((error) => {
+                reject(error);
+            });
+    });
 };
 
-const fetchAzureAPI = (image, token) => {
+const fetchAzureAPI = (image) => {
     new Promise((resolve, reject) => {
-        const url = new URL();  //URL here
-        opts.headers = opts.headers || {};
-        opts.headers.Authorization = `Bearer ${token}`; //Authorization whatever it is
-
-        if (opts.params) {
-            if (opts.params instanceof Object || opts.params instanceof Array) {
-                url.search = new URLSearchParams(opts.params);
+        cv_client.analyzeImage(image, {
+            visualFeatures: ['Description']
+        })
+        .then((image_analysis) => {
+            if (image_analysis.description.captions.length > 0) {
+                const captions = image_analysis.description.captions;
+                const first_caption = captions[0].text;
+                translate(first_caption, { from: 'en', to: 'es' })
+                .then((translated_caption) => {
+                    resolve(translated_caption);
+                })
+                .catch((error) => {
+                    reject(error);
+                });
             } else {
-                url.search = new URLSearchParams(Object.assign({}, opts.params));
+                const with_tags = image_analysis.description.tags.slice(0, 3).join(', ');
+                translate(with_tags, { from: 'en', to: 'es' })
+                .then((translated_caption) => {
+                    resolve(translated_caption);
+                })
+                .catch((error) => {
+                    reject(error);
+                });
             }
-        }
-
-        return fetch(url, opts)
-            .then(response => {
-                if (response.status === 401) {
-                    return response
-                        .json()
-                        .then(body => {
-                            const err = new Error(`Request failed: ${body.msg}`);
-                            err.code = '401';
-                            error(err);
-                            reject(err);
-                        })
-                        .catch(err => reject(err));
-                }
-
-                if (response.ok) {
-                    const content_type = response.headers.get('content-type');
-                    if (content_type && content_type.indexOf('application/json' !== -1)) {
-                        return response
-                            .json()
-                            .then(body => resolve(body))
-                            .catch(err => reject(err));
-                    }
-
-                    // If the response is not a JSON we consider it an emtpy response
-                    return resolve();
-                }
-
-                return response
-                    .json()
-                    .then(body => {
-                        const error = new Error(`Request failed: ${body.msg}`);
-                        error.code = response.status;
-                        error(error);
-                        reject(error);
-                    })
-                    .catch(err => reject(err));
-            })
-            .catch(err => reject(err))
-    })
+        })
+        .catch((error) => {
+            reject(error);
+        });
+    });
 };
 
-export const requestAllAPIs = (image, token) => {
+export const requestAllAPIs = (image) => {
     Promise.all([
-        fetchAmazonAPI(image, token),
-        fetchGoogleAPI(image, token),
-        fetchAzureAPI(image, token)
+        fetchAmazonAPI(image),
+        fetchGoogleAPI(image),
+        fetchAzureAPI(image)
     ])
         .then(results => cb(null, transformResponsesToImageDescriptions(image, results) || []))
         .catch(err => cb(err));
